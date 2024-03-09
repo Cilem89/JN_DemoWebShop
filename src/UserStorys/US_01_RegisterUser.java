dd8447174a963243087cf7cfb52055c57af9ea1a
cilem89 <okkalicilem@gmail.com>
2024-03-09 15:06:21 +0300

    US_01_RegisterUser tamamlandı

--- src/UserStorys/US_01_RegisterUser.java
+++ src/UserStorys/US_01_RegisterUser.java
@@ -6,6 +6,7 @@ import com.google.common.annotations.VisibleForTesting;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class US_01_RegisterUser extends BaseDriver {
 
@@ -28,6 +29,17 @@ public class US_01_RegisterUser extends BaseDriver {
 
         WebElement inputEmail=driver.findElement(By.id("Email"));
         inputEmail.sendKeys("javanator@gmail.com");
+
+        WebElement inputPassword=driver.findElement(By.id("Password"));
+        inputPassword.sendKeys("Pass123");
+
+        WebElement inputPassword2=driver.findElement(By.id("ConfirmPassword"));
+        inputPassword2.sendKeys("Pass123");
+
+        WebElement registerBtn= driver.findElement(By.id("register-button"));
+        registerBtn.click();
+
+        BekleKapat();
 
     }
 }
